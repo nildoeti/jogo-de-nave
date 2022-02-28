b9372f7518d9a015863515cf2d736dc65aec34a2
Nildo <nildo@webstack.com.br>
2022-02-27 21:06:26 -0300

    adicionado class anima2 ao script

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ function start() { // início da função start
     $("#inicio").hide();
 
     $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2' ></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo'></div>");
 } // fim da função start
