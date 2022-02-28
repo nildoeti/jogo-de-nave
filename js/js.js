ad99e17dbd2a5c275e46703c1819068ad6da3ea0
Nildo <nildo@webstack.com.br>
2022-02-27 23:59:34 -0300

    adicionado função para mover o fundo do cenário do jogo

--- js/js.js
+++ js/js.js
@@ -5,4 +5,25 @@ function start() { // início da função start
     $("#fundoGame").append("<div id='inimigo1' class='anima2' ></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    //Principais variáveis do jogo
+	var jogo = {}
+	
+	//Loop do jogo
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+        movefundo();
+	
+	} // Fim da função loop()
+
+
+    //Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+	    $("#fundoGame").css("background-position",esquerda-1);
+	
+	} // fim da função movefundo()
+
 } // fim da função start
