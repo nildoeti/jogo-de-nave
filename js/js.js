b4e65bb2dfe28914edfa0291d3fab006f333020e
Nildo <nildo@webstack.com.br>
2022-02-27 22:00:35 -0300

    adicionado class anima3 para amigo do projeto

--- js/js.js
+++ js/js.js
@@ -4,5 +4,5 @@ function start() { // início da função start
     $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
     $("#fundoGame").append("<div id='inimigo1' class='anima2' ></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 } // fim da função start
