83bc000d6afc4c3f34a126e621f0ebd24e1b0ce7
Nildo <nildo@webstack.com.br>
2022-03-02 19:54:16 -0300

    adicionado configuração de teclas do jogo

--- js/js.js
+++ js/js.js
@@ -7,23 +7,33 @@ function start() { // início da função start
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //Principais variáveis do jogo
-	var jogo = {}
-	
+	var jogo = {};
+
+    // definindo as teclas do jogo
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    };
+
+    jogo.pressionou = [];
+
+
 	//Loop do jogo
 	jogo.timer = setInterval(loop,30);
-	
+
 	function loop() {
         movefundo();
-	
-	} // Fim da função loop()
+
+	} // Fim da função loop()  
 
 
     //Função que movimenta o fundo do jogo
-	
+
 	function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
 	    $("#fundoGame").css("background-position",esquerda-1);
-	
+
 	} // fim da função movefundo()
 
 } // fim da função start
