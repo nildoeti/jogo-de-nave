267685ec61e3ea5807a81050fb4790271f6306e9
Nildo <nildo@webstack.com.br>
2022-03-03 09:34:55 -0300

    finalizado script para controle do disparo do helicoptero

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() { // início da função start
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //Principais variáveis do jogo
+    var podeAtirar=true;
 	var jogo = {};
 
     var velocidade=5;
@@ -87,6 +88,7 @@ function start() { // início da função start
     if (jogo.pressionou[TECLA.D]) {
         
         //Chama função Disparo  
+        disparo();
     }
 
     } // fim da função movejogador()
@@ -135,3 +137,37 @@ function moveamigo() {
 
 } // fim da função moveamigo()
 
+
+
+function disparo() {
+    
+    if (podeAtirar==true) {
+        
+    podeAtirar=false;
+    
+    topo = parseInt($("#jogador").css("top"))
+    posicaoX= parseInt($("#jogador").css("left"))
+    tiroX = posicaoX + 190;
+    topoTiro=topo+37;
+    $("#fundoGame").append("<div id='disparo'></div");
+    $("#disparo").css("top",topoTiro);
+    $("#disparo").css("left",tiroX);
+    
+    var tempoDisparo=window.setInterval(executaDisparo, 30);
+    
+    } //Fecha podeAtirar
+ 
+        function executaDisparo() {
+        posicaoX = parseInt($("#disparo").css("left"));
+        $("#disparo").css("left",posicaoX+15); 
+
+                if (posicaoX>900) {
+                        
+            window.clearInterval(tempoDisparo);
+            tempoDisparo=null;
+            $("#disparo").remove();
+            podeAtirar=true;
+                    
+                   }
+    } // Fecha executaDisparo()
+} // Fecha disparo()
